29cb04043cf9fc649f810ce1bf8045ed6e5db69f
Ahmet Enes Bayraktar <bayraktarahmetenes@gmail.com>
2018-12-25 01:58:02 +0300

    test string for arithmetic ops

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace netSchematron
             BuildResult<Parser<XPathToken, object>> buildResult = builder.BuildParser(xpathParser, ParserType.EBNF_LL_RECURSIVE_DESCENT, "XPath");
 
             Parser<XPathToken, object> parser = buildResult.Result;
-            ParseResult<XPathToken, object> parseResult = parser.Parse("---1-2----((3--2)++(-4+3))", "XPath");
+            ParseResult<XPathToken, object> parseResult = parser.Parse("---1-2 div 5---(2*--(3--2)idiv 3 ++(-4+3) * 5) div 5", "XPath");
         }
     }
 }
